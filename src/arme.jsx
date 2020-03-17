8627b374e7965c5cd933a6b6a0007c718f51a3de
Florent JUIF <florentjuif@gmail.com>
2020-03-17 16:46:20 +0100

    nom + image

--- src/arme.jsx
+++ src/arme.jsx
@@ -2,6 +2,10 @@ import React from "react";
 
 
 const Arme = ({ nom, image, onClick, selected }) => {
+   nom='AK'
+   image='https://www.airsoft-entrepot.fr/media/catalog/product/cache/0cc4f0dcb6b953b8f548409f40d73049/c/m/cm028_002.jpg'
+   
+
   return (
     <div className={("arme",{selected})} onClick={onClick}>
       {nom}
